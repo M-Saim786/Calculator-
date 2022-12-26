e05a49c6f1d530e59c8f920c138accaff26f7aa7
M-Saim786 <saimrashid3344@gmail.com>
2022-12-26 17:18:30 +0500

    Final

--- app.js
+++ app.js
@@ -1,45 +1,17 @@
-// sir ka code
-/* var inp = document.getElementById("text")
-function showdata(e){
-    console.log(e.innerText)
-    inp.value += e.innerText
-
-
-}
-
-function getrest(){
-    var res = eval(inp.value)
-    inp.value=res
-}
-
-function clr(){
-    inp.value=""
-}
-
-function bksp(){
-    inp.value= inp.value.substring(0,inp.value.length-1)
-}
-
-function sqrt(){
-    inp.value= Math.sqrt(inp.value)
-}
-
-function sqr(){
-    inp.value =inp.value*inp.value
-}
-*/
-
 var screen = document.getElementById("result");
 function showdata(e) {
-  console.log(e.innerText);
+  // console.log(e.innerText);
   screen.value += e.innerText;
 }
+// Clear Once Func
+var clearOne = document.getElementById("clearOne");
+clearOne.addEventListener("click", () => {
+  screen.value = screen.value.substring(0, screen.value.length - 1);
+});
+
+// Clear All Func
 function clearall() {
   screen.value = "";
-}
-
-function clear() {
-  console.log("click");
 }
 
 function multiply(e) {
